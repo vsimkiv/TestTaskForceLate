faeed88c41ce5bb09498cb2575ff125ac1e19908
Volodymyr Simkiv <volodymyr.simkiv@gmail.com>
2019-04-01 11:35:45 +0300

    add gitignore file. Create SpringBooT project

---
+++ forcelate/src/main/java/com/simkiv/forcelate/ForcelateApplication.java
@@ -0,0 +1,13 @@
+package com.simkiv.forcelate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForcelateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForcelateApplication.class, args);
+    }
+
+}
